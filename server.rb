4a6d9ab9a6fbba075988b51c9cd4f0dcf0893b55
Keith Webber <keith.e.webber@gmail.com>
2014-03-13 16:06:34 -0400

    set up basic views

--- server.rb
+++ server.rb
@@ -1,3 +1,21 @@
 require 'sinatra'
 require 'csv'
 require 'pry'
+
+
+#home page request
+get '/' do
+  erb :index
+end
+
+get '/bus' do
+  erb :bus
+end
+
+get '/train' do
+  erb :train
+end
+
+get '/bus_train' do
+  erb :bus_train
+end
